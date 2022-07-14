a3af9cff044600ed59234e8f97852201c8f9b456
TuanNguyenDin <89063829+TuanNguyenDin@users.noreply.github.com>
2022-07-14 16:18:07 +0700

    React Redux Forms Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,255 +1,77 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
 import { Breadcrumb, BreadcrumbItem,
-  Button, Row, Col, Label } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+        Button, Row, Col, Label } from 'reactstrap';
+import { Control, Form, Errors } from 'react-redux-form';
+import { Link } from 'react-router-dom';
+
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 const isNumber = (val) => !isNaN(Number(val));
 const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
-
-
-// function Contact(props) {
-//   return (
-//     <div className="container">
-//       <div className="row">
-//         <Breadcrumb>
-//           <BreadcrumbItem>
-//             <Link to="/home">Home</Link>
-//           </BreadcrumbItem>
-//           <BreadcrumbItem active>Contact Us</BreadcrumbItem>
-//         </Breadcrumb>
-//         <div className="col-12">
-//           <h3>Contact Us</h3>
-//           <hr />
-//         </div>
-//       </div>
-//       <div className="row row-content">
-//         <div className="col-12">
-//           <h3>Location Information</h3>
-//         </div>
-//         <div className="col-12 col-sm-4 offset-sm-1">
-//           <h5>Our Address</h5>
-//           <address>
-//             121, Clear Water Bay Road
-//             <br />
-//             Clear Water Bay, Kowloon
-//             <br />
-//             HONG KONG
-//             <br />
-//             <i className="fa fa-phone"></i>: +852 1234 5678
-//             <br />
-//             <i className="fa fa-fax"></i>: +852 8765 4321
-//             <br />
-//             <i className="fa fa-envelope"></i>:{" "}
-//             <a href="mailto:confusion@food.net">confusion@food.net</a>
-//           </address>
-//         </div>
-//         <div className="col-12 col-sm-6 offset-sm-1">
-//           <h5>Map of our Location</h5>
-//           <iframe
-//             title="address"
-//             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.5236503473993!2d114.21604661542818!3d22.333848147318914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x340406b77660041b%3A0x1e1faa9bb38e36f3!2s121%20Clear%20Water%20Bay%20Rd%2C%20Ngau%20Chi%20Wan%2C%20Hong%20Kong!5e0!3m2!1sen!2s!4v1656124059314!5m2!1sen!2s"
-//             width="600"
-//             height="450"
-//             allowfullscreen=""
-//             loading="lazy"
-//             referrerpolicy="no-referrer-when-downgrade"
-//             className="google-map"
-//           ></iframe>
-//         </div>
-//         <div className="col-12 col-sm-11 offset-sm-1">
-//           <div className="btn-group" role="group">
-//             <a
-//               role="button"
-//               className="btn btn-primary"
-//               href="tel:+85212345678"
-//             >
-//               <i className="fa fa-phone"></i> Call
-//             </a>
-//             <a role="button" className="btn btn-info">
-//               <i className="fa fa-skype"></i> Skype
-//             </a>
-//             <a
-//               role="button"
-//               className="btn btn-success"
-//               href="mailto:confusion@food.net"
-//             >
-//               <i className="fa fa-envelope-o"></i> Email
-//             </a>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Contact;
 class Contact extends Component {
-  constructor(props) {
-    super(props);
-    
 
-    this.state = {
-        firstname: '',
-        lastname: '',
-        telnum: '',
-        email: '',
-        agree: false,
-        contactType: 'Tel.',
-        message: '',
-        touched: {
-            firstname: false,
-            lastname: false,
-            telnum: false,
-            email: false
-        }
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
-}
 
+    handleSubmit(values) {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
+        // event.preventDefault();
+    }
 
-handleSubmit(values) {
-  console.log('Current State is: ' + JSON.stringify(values));
-  alert('Current State is: ' + JSON.stringify(values));
-  // event.preventDefault();
-}
-
-
-handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-}
-
-// handleSubmit(event) {
-//     console.log('Current State is: ' + JSON.stringify(this.state));
-//     alert('Current State is: ' + JSON.stringify(this.state));
-//     event.preventDefault();
-// }
-
-handleBlur = (field) => (evt) => {
-  this.setState({
-      touched: { ...this.state.touched, [field]: true }
-  });
-}
-
-validate(firstname, lastname, telnum, email) {
-  const errors = {
-      firstname: '',
-      lastname: '',
-      telnum: '',
-      email: ''
-  };
-
-  if (this.state.touched.firstname && firstname.length < 3)
-      errors.firstname = 'First Name should be >= 3 characters';
-  else if (this.state.touched.firstname && firstname.length > 10)
-      errors.firstname = 'First Name should be <= 10 characters';
-
-  if (this.state.touched.lastname && lastname.length < 3)
-      errors.lastname = 'Last Name should be >= 3 characters';
-  else if (this.state.touched.lastname && lastname.length > 10)
-      errors.lastname = 'Last Name should be <= 10 characters';
-
-  const reg = /^\d+$/;
-  if (this.state.touched.telnum && !reg.test(telnum))
-      errors.telnum = 'Tel. Number should contain only numbers';
-
-  if(this.state.touched.email && email.split('').filter(x => x === '@').length !== 1)
-      errors.email = 'Email should contain a @';
-
-  return errors;
-}
-
-  
-  render() {
-    const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
-      return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/home">Home</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>Contact Us</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>Contact Us</h3>
-          <hr />
-        </div>
-      </div>
-      <div className="row row-content">
-        <div className="col-12">
-          <h3>Location Information</h3>
-        </div>
-        <div className="col-12 col-sm-4 offset-sm-1">
-          <h5>Our Address</h5>
-          <address>
-            121, Clear Water Bay Road
-            <br />
-            Clear Water Bay, Kowloon
-            <br />
-            HONG KONG
-            <br />
-            <i className="fa fa-phone"></i>: +852 1234 5678
-            <br />
-            <i className="fa fa-fax"></i>: +852 8765 4321
-            <br />
-            <i className="fa fa-envelope"></i>:{" "}
-            <a href="mailto:confusion@food.net">confusion@food.net</a>
-          </address>
-        </div>
-        <div className="col-12 col-sm-6 offset-sm-1">
-          <h5>Map of our Location</h5>
-          <iframe
-            title="address"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.5236503473993!2d114.21604661542818!3d22.333848147318914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x340406b77660041b%3A0x1e1faa9bb38e36f3!2s121%20Clear%20Water%20Bay%20Rd%2C%20Ngau%20Chi%20Wan%2C%20Hong%20Kong!5e0!3m2!1sen!2s!4v1656124059314!5m2!1sen!2s"
-            width="600"
-            height="450"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-            className="google-map"
-          ></iframe>
-        </div>
-        <div className="col-12 col-sm-11 offset-sm-1">
-          <div className="btn-group" role="group">
-            <a
-              role="button"
-              className="btn btn-primary"
-              href="tel:+85212345678"
-            >
-              <i className="fa fa-phone"></i> Call
-            </a>
-            <a role="button" className="btn btn-info">
-              <i className="fa fa-skype"></i> Skype
-            </a>
-            <a
-              role="button"
-              className="btn btn-success"
-              href="mailto:confusion@food.net"
-            >
-              <i className="fa fa-envelope-o"></i> Email
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="row row-content">
-                   <div className="col-12">
-                      <h3>Send us your Feedback</h3>
-                   </div>
-                    <div className="col-12 col-md-9">
-                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-                    <Row className="form-group">
+    render() {
+        //const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
+        return(
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3 class="text-secondary">Contact Us</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row row-content">
+                    <div className="col-12">
+						<h3 class="text-secondary">Location Information</h3>
+                    </div>
+                    <div className="col-12 col-sm-4 offset-sm-1">
+						<h5>Our Address</h5>
+						<address>
+                            121, Clear Water Bay Road<br />
+                            Clear Water Bay, Kowloon<br />
+                            HONG KONG<br />
+                            <i className="fa fa-phone"></i>: +852 1234 5678<br />
+                            <i className="fa fa-fax"></i>: +852 8765 4321<br />
+                            <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+						</address>
+                    </div>
+                    <div className="col-12 col-sm-6 offset-sm-1">
+                        <h5>Map of our Location</h5>
+                    </div>
+                    <div className="col-12 col-sm-11 offset-sm-1">
+                        <div className="btn-group" role="group">
+                            <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                            <a role="button" className="btn btn-info" href="/"><i className="fa fa-skype"></i> Skype</a>
+                            <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h3 class="text-secondary">Send us your Feedback</h3>
+                    </div>
+					<div className="col-12 col-md-9">
+                        {/* <LocalForm onSubmit={(values) => this.handleSubmit(values)}> */}
+                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
                                     <Control.text model=".firstname" id="firstname" name="firstname"
@@ -337,15 +159,14 @@ validate(firstname, lastname, telnum, email) {
                                      />
                                 </Col>
                             </Row>
-
                             <Row className="form-group">
                                 <Col md={{size: 6, offset: 2}}>
                                     <div className="form-check">
                                         <Label check>
                                             <Control.checkbox model=".agree" name="agree"
                                                 className="form-check-input"
-                                                 /> {' '}
-                                                <strong>May we contact you?</strong>
+											/> {' '}
+											<strong>May we contact you?</strong>
                                         </Label>
                                     </div>
                                 </Col>
@@ -362,26 +183,24 @@ validate(firstname, lastname, telnum, email) {
                                 <Col md={10}>
                                     <Control.textarea model=".message" id="message" name="message"
                                         rows="12"
-                                        className="form-control" />
+                                        className="form-control"
+									/>
                                 </Col>
                             </Row>
                             <Row className="form-group">
                                 <Col md={{size:10, offset: 2}}>
                                     <Button type="submit" color="primary">
-                                    Send Feedback
+                                        Send Feedback
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
-
-                    </div>
-               </div>
-
-    </div>
-  );
-
-    
-  }
-
+                        </Form>
+                        {/* </LocalForm> */}
+					</div>
+                </div>
+            </div>
+        )
+    };
 }
+
 export default Contact;
